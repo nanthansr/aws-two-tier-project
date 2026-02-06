3bdae66f6c75aa2d22e9cc18dbb27ccc252aefb4
nanthansr <nanthansr@gmail.com>
2026-02-06 17:48:01 -0500

    fix: update endpoints to cloud IP

--- frontend/src/endpoints.js
+++ frontend/src/endpoints.js
@@ -1,5 +1,5 @@
 //const BASEURL = "https://extented-node-blog-backend.herokuapp.com/";
-const BASEURL = "http://localhost:8000/";
+const BASEURL = "http://3.135.248.151:8000/";
 export const endpoint = BASEURL + "blogs";
 export const signup = BASEURL + "users";
 export const login = BASEURL + "token";
